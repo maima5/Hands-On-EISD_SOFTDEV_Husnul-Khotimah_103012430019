2b3bfcbf48b0b641a87b64f35eb51b69a96a83ae
Imaa <husnulkhotimah@student.telkomuniversity.ac.id>
2026-02-12 00:44:39 +0700

    Refactor main function to use Peserta class

--- Soal5.js
+++ Soal5.js
@@ -1,12 +1,24 @@
+class Peserta {
+    constructor(divisi, jumlah) {
+        this.Divisi = divisi;
+        this.Jumlah = jumlah;
+    }
+}
+
 function main() {
-    const peserta = [18, 15, 10, 10];
+    const data = [
+        new Peserta("Web Development Dasar", 18),
+        new Peserta("Logic & Algorithm", 15),
+        new Peserta("Frontend Fundamental", 10),
+        new Peserta("Backend Introduction", 10)
+    ];
 
     let total = 0;
     for (let i = 0; i < 4; i++) {
-        total = total + peserta[i];
+        total = total + data[i].Jumlah;
     }
 
-    const rataRata = total / peserta.length;
+    const rataRata = total / data.length;
 
     console.log("Total seluruh peserta:", total);
     console.log("Rata-rata peserta per Study Group:", rataRata);
